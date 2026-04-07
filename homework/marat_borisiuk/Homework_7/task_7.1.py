15f150a938ff94c95427124a022373966eff59ef
maratbv <204992045+maratbv@users.noreply.github.com>
2026-04-07 21:35:55 +0300

    Homework_7 done

---
+++ homework/marat_borisiuk/Homework_7/task_7.1.py
@@ -0,0 +1,8 @@
+number = 1
+while True:
+    user_input = int(input())
+    if user_input == number:
+        print('Поздравляю! Вы угадали!')
+        break
+    else:
+        print('попробуйте снова')
