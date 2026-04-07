ba0865039bf5fd0eb27b53dcfc0f4c2af080d7f9
maratbv <204992045+maratbv@users.noreply.github.com>
2026-04-07 21:41:46 +0300

    fixed formatting

--- homework/marat_borisiuk/Homework_7/task_7.3.py
+++ homework/marat_borisiuk/Homework_7/task_7.3.py
@@ -3,10 +3,12 @@ result_2 = 'результат операции: 54'
 result_3 = 'результат работы программы: 209'
 result_4 = 'результат: 2'
 
+
 def number_from_result(text):
     words = text.split()
     number = int(words[-1]) + 10
     return number
+
 
 print(number_from_result(result_1))
 print(number_from_result(result_2))
