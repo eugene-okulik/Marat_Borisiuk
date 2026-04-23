10b9e33befb01c08756503fb2dbc92d74345ca77
maratbv <204992045+maratbv@users.noreply.github.com>
2026-04-23 13:33:00 +0300

    Changed the name of the class Flower, changed the class Roses, changed the search method, changed the sorting methods

--- homework/marat_borisiuk/Homework_12/task_12.1.py
+++ homework/marat_borisiuk/Homework_12/task_12.1.py
@@ -1,4 +1,4 @@
-class Flowers:
+class Flower:
 
     def __init__(self, name, colour, length, price, avg_lifetime):
         self.name = name
@@ -8,29 +8,32 @@ class Flowers:
         self.avg_lifetime = avg_lifetime
 
 
-class Roses(Flowers):
-    pass
+class Roses(Flower):
+    def __init__(self, colour, length, price, avg_lifetime):
+        super().__init__('rose', colour, length, price, avg_lifetime)
 
 
-class Irises(Flowers):
-    pass
+class Irises(Flower):
+    def __init__(self, colour, length, price, avg_lifetime):
+        super().__init__('iris', colour, length, price, avg_lifetime)
 
 
-class Peonies(Flowers):
-    pass
+class Peonies(Flower):
+    def __init__(self, colour, length, price, avg_lifetime):
+        super().__init__('peony', colour, length, price, avg_lifetime)
 
 
-rose_1 = Roses('rose', 'red', 50, 5, 4)
-rose_2 = Roses('rose', 'white', 45, 4.5, 4)
-rose_3 = Roses('rose', 'yellow', 40, 4, 4)
+rose_1 = Roses('red', 50, 5, 4)
+rose_2 = Roses('white', 45, 4.5, 4)
+rose_3 = Roses('yellow', 40, 4, 4)
 
-iris_1 = Irises('iris', 'blue', 20, 4, 6)
-iris_2 = Irises('iris', 'blue', 18, 4, 6)
-iris_3 = Irises('iris', 'blue', 15, 4, 6)
+iris_1 = Irises('blue', 20, 4, 6)
+iris_2 = Irises('blue', 18, 4, 6)
+iris_3 = Irises('blue', 15, 4, 6)
 
-peony_1 = Peonies('peony', 'white', 50, 6.5, 12)
-peony_2 = Peonies('peony', 'red', 55, 6.5, 12)
-peony_3 = Peonies('peony', 'red', 55, 6.5, 12)
+peony_1 = Peonies('white', 50, 6.5, 12)
+peony_2 = Peonies('red', 55, 6.5, 12)
+peony_3 = Peonies('red', 55, 6.5, 12)
 
 
 class Bouquet():
@@ -50,29 +53,32 @@ class Bouquet():
             total_lifetime += flower.avg_lifetime
         return total_lifetime / len(self.bouquet_list)
 
-    def sort_by_avg_lifetime(self):
-        self.bouquet_list.sort(key=lambda flower: flower.avg_lifetime)
+    # def sort_by_avg_lifetime(self):
+    #     self.bouquet_list.sort(key=lambda flower: flower.avg_lifetime)
+    #
+    # def sort_by_color(self):
+    #     self.bouquet_list.sort(key=lambda flower: flower.colour)
+    #
+    # def sort_by_length(self):
+    #     self.bouquet_list.sort(key=lambda flower: flower.length)
+    #
+    # def sort_by_price(self):
+    #     self.bouquet_list.sort(key=lambda flower: flower.price)
 
-    def sort_by_color(self):
-        self.bouquet_list.sort(key=lambda flower: flower.colour)
+    def sort_by(self, key):
+        self.bouquet_list.sort(key=lambda flower: getattr(flower, key))
 
-    def sort_by_length(self):
-        self.bouquet_list.sort(key=lambda flower: flower.length)
-
-    def sort_by_price(self):
-        self.bouquet_list.sort(key=lambda flower: flower.price)
-
-    def search_by_avg_lifetime(self):
+    def search_by_avg_lifetime(self, avg_lifetime):
         found_flowers = []
         for flower in self.bouquet_list:
-            if flower.avg_lifetime == 12:
+            if avg_lifetime == flower.avg_lifetime:
                 found_flowers.append(flower)
         return found_flowers
 
-    def search_by_colour(self):
+    def search_by_colour(self, colour):
         found_flowers = []
         for flower in self.bouquet_list:
-            if flower.colour == 'red':
+            if flower.colour == colour:
                 found_flowers.append(flower)
         return found_flowers
 
