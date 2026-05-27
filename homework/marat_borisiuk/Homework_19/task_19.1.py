f92b67e541827c61d03d11dd7067bb510ce9b687
maratbv <204992045+maratbv@users.noreply.github.com>
2026-05-27 13:03:35 +0300

    Added object creation and deletion functions. Fixed the PATCH function

--- homework/marat_borisiuk/Homework_19/task_19.1.py
+++ homework/marat_borisiuk/Homework_19/task_19.1.py
@@ -2,24 +2,31 @@ import requests
 
 
 def create_object():
-    body = {"data": {"color": "white", "size": "big"}, "name": "First object"}
-    response = requests.post(
-        "http://objapi.course.qa-practice.com/object", json=body
-    ).json()
-    assert "id" in response, "Object creation failed"
-    obj_id = response["id"]
-    response = requests.get(f"http://objapi.course.qa-practice.com/object/{obj_id}")
-    assert response.status_code == 200, "Object creation failed"
-    requests.delete(f"http://objapi.course.qa-practice.com/object/{obj_id}")
-
-
-def put_object():
-    body = {"data": {"color": "white", "size": "big"}, "name": "First object"}
+    body = {"data": {"color": "white", "size": "big"}, "name": "Create object"}
     create_response = requests.post(
         "http://objapi.course.qa-practice.com/object", json=body
     ).json()
+    assert "id" in create_response, "Object creation failed"
     obj_id = create_response["id"]
+    return obj_id
 
+
+def clear_object(obj_id):
+    clear_response = requests.delete(
+        f"http://objapi.course.qa-practice.com/object/{obj_id}"
+    )
+    assert clear_response.status_code in [200, 204], "Object deletion failed"
+
+
+def post_object():
+    obj_id = create_object()
+    response = requests.get(f"http://objapi.course.qa-practice.com/object/{obj_id}")
+    assert response.status_code == 200, "Object creation failed"
+    clear_object(obj_id)
+
+
+def put_object():
+    obj_id = create_object()
     update_body = {
         "data": {"color": "black", "size": "bigger"},
         "name": "First object updated",
@@ -28,41 +35,25 @@ def put_object():
         f"http://objapi.course.qa-practice.com/object/{obj_id}", json=update_body
     ).json()
     assert response["data"]["size"] == "bigger", "Object update failed"
-    requests.delete(f"http://objapi.course.qa-practice.com/object/{obj_id}")
+    clear_object(obj_id)
 
 
 def patch_object():
-    body = {"data": {"color": "white", "size": "big"}, "name": "First object"}
-    create_response = requests.post(
-        "http://objapi.course.qa-practice.com/object", json=body
-    ).json()
-    obj_id = create_response["id"]
-
-    patch_body = {
-        "data": {"color": "black", "size": "bigger"},
-        "name": "Patch First object",
-    }
+    obj_id = create_object()
+    patch_body = {"name": "Patch First object"}
     response = requests.patch(
         f"http://objapi.course.qa-practice.com/object/{obj_id}", json=patch_body
     ).json()
     assert response["name"] == "Patch First object", "Object patch failed"
-    requests.delete(f"http://objapi.course.qa-practice.com/object/{obj_id}")
+    clear_object(obj_id)
 
 
 def delete_object():
-    body = {"data": {"color": "white", "size": "big"}, "name": "First object"}
-    create_response = requests.post(
-        "http://objapi.course.qa-practice.com/object", json=body
-    ).json()
-    obj_id = create_response["id"]
-
-    delete_response = requests.delete(
-        f"http://objapi.course.qa-practice.com/object/{obj_id}"
-    )
-    assert delete_response.status_code in [200, 204], "Object deletion failed"
+    obj_id = create_object()
+    clear_object(obj_id)
 
 
-create_object()
+post_object()
 put_object()
 patch_object()
 delete_object()
